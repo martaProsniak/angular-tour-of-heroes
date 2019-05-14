bf17643ade952e22fcf7125c852e2e5fc1d65d8c
Marta <martycjas13@gmail.com>
2019-05-14 16:39:37 +0200

    routing finished

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {Hero} from '../hero';
-import { HeroService} from '../hero.service';
+
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,30 +10,15 @@ import { HeroService} from '../hero.service';
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
+
   constructor(private heroService: HeroService) { }
-  selectedHero: Hero;
 
   ngOnInit() {
     this.getHeroes();
   }
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
   getHeroes(): void {
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes);
   }
-
 }
- /*
- Observable.subscribe() is the critical difference.
-
-The previous version assigns an array of heroes to the component's heroes property. The assignment occurs synchronously, as if the server could return heroes instantly or the browser could freeze the UI while it waited for the server's response.
-
-That won't work when the HeroService is actually making requests of a remote server.
-
-The new version waits for the Observable to emit the array of heroes— which could happen now or several minutes from now. Then subscribe passes the emitted array to the callback, which sets the component's heroes property.
-
-This asynchronous approach will work when the HeroService requests heroes from the server.
-*/
